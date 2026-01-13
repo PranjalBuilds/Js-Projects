f64764f822bcf760e788858a0fb094eda409e57c
PranjalBuilds <imcae22060@gmail.com>
2026-01-13 23:51:41 +0530

    01. Drumkit ready

--- 01/script.js
+++ 01/script.js
@@ -1,1 +1,24 @@
-let dataKey = [65, 83, 68, 70, 71, 72, 74, 75, 76] // A, S, D, F, G, H, J, K, L
+const keys = document.querySelectorAll('.key');
+
+
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    
+    // console.log(audio);
+    if (!audio) return;
+    
+    audio.currentTime = 0;
+    audio.play();
+    
+    // console.log(key);
+    key.classList.add('playing');
+}
+function removeTransition (e) {
+    if(e.propertyName !== 'transform') return;
+    // console.log(e.propertyName);
+    this.classList.remove('playing');
+}
+
+window.addEventListener('keydown', playSound);
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
