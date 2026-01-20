317c2f6318e2021318635af2acbfba341e4ec514
PranjalBuilds <imcae22060@gmail.com>
2026-01-20 09:36:06 +0530

    04. Reduce

--- 04/data.js
+++ 04/data.js
@@ -13,3 +13,89 @@ export const inventors = [
     { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
 ];
 
+export const users =[
+  {
+    "id": 1,
+    "name": "Alice Johnson",
+    "isActive": true
+  },
+  {
+    "id": 2,
+    "name": "Bob Smith",
+    "isActive": false
+  },
+  {
+    "id": 3,
+    "name": "Charlie Davis",
+    "isActive": true
+  }
+]
+
+export const cart = [
+  {
+    "id": 101,
+    "name": "Wireless Headphones",
+    "price": 2499,
+    "qty": 1,
+    "paymentStatus": "Paid"
+  },
+  {
+    "id": 102,
+    "name": "Mechanical Keyboard",
+    "price": 4500,
+    "qty": 1,
+    "paymentStatus": "Failed"
+  },
+  {
+    "id": 103,
+    "name": "USB-C Fast Charger",
+    "price": 899,
+    "qty": 2,
+    "paymentStatus": "Paid"
+  },
+  {
+    "id": 104,
+    "name": "Ergonomic Mouse Pad",
+    "price": 550,
+    "qty": 1,
+    "paymentStatus": "Failed"
+  },
+  {
+    "id": 105,
+    "name": "Bluetooth Speaker",
+    "price": 1299,
+    "qty": 1,
+    "paymentStatus": "Paid"
+  }
+]
+
+export const usersCountry = [
+    {
+      "name": "Arjun Mehta",
+      "country": "India"
+    },
+    {
+      "name": "Sophia Müller",
+      "country": "Germany"
+    },
+    {
+      "name": "Yuki Tanaka",
+      "country": "Japan"
+    },
+    {
+      "name": "Mateo Garcia",
+      "country": "Spain"
+    },
+    {
+      "name": "Amina Okoro",
+      "country": "Nigeria"
+    }
+  ]
+
+export const products = [
+    { id: 101, name: "Wireless Headphones", price: 2499 },
+    { id: 102, name: "Mechanical Keyboard", price: 4500 },
+    { id: 103, name: "USB-C Fast Charger", price: 899 },
+    { id: 104, name: "Ergonomic Mouse Pad", price: 550 },
+    { id: 105, name: "Bluetooth Speaker", price: 1299 }
+]
--- 04/script.js
+++ 04/script.js
@@ -1,6 +1,8 @@
 import { inventors } from "./data.js";
-
-
+import { users } from "./data.js";
+import { cart } from "./data.js";
+import { usersCountry } from "./data.js";
+import { products } from "./data.js";
 
 // Map, Filter, ForEach
 
@@ -15,3 +17,129 @@ const born1500 = inventors
 
 const ordered = inventors.sort((a, b) => a.year > b.year ? 1 : -1);
 console.log(ordered);
+
+
+
+// Reduce
+
+// Traditional way with loops
+
+let totalYear = 0;
+
+for (let i = 0; i < inventors.length; i++) {
+    totalYear += inventors[i].year;
+}
+
+console.log(totalYear);
+
+
+//with reduce
+
+const totalYears = inventors.reduce((total, inventor) => {
+    return total + (inventor.passed - inventor.year);
+}, 0);
+
+console.log(totalYears);
+
+
+// active users
+
+const activeUsers = users.reduce((acc, u) => {
+    if (u.isActive) {
+        return acc === "" ? u.name : `\n${acc},\n${u.name}\n`;
+    }
+    return acc;
+}, "");
+
+console.log(activeUsers);
+
+
+// traditional way for 'cart' iteration
+
+let total = 0
+for (const product of cart) {
+    total += product.price * product.qty;
+    
+}
+console.log(`total of cart products : ${total}`);
+
+
+// with reduce
+
+const totalCart = cart.reduce((acc, product) => {
+    return acc + product.price * product.qty;
+    // return acc;
+}, 0);
+
+console.log(`total of cart products (using Reduce) : ${totalCart}`);
+
+
+// Grouping data by a property (using Reduce)
+
+// Should look like :
+/*
+
+{
+    India: [
+        {
+            name: "Arjun Mehta", 
+            country: "India"
+        },
+    ]
+    Germany: [
+        {
+            name: "Sophia Müller", 
+            country: "Germany"
+        },
+    ]
+    Japan: [
+        {
+            name: "Yuki Tanaka", 
+            country: "Japan"
+        },
+    ]
+    
+    ..etc
+}
+
+*/ 
+
+
+const usersCountryGrouped = usersCountry.reduce((acc, user) => {
+    acc[user.country] = acc[user.country] || [];
+    acc[user.country].push(user);
+    return acc;
+}, {})
+
+console.log(usersCountryGrouped);
+
+
+// Creating look up table or index map for faster retrieval of data
+
+/*
+
+{
+    1: {
+        id: 1,
+        name: "Product 1",
+        price: 100,
+        qty: 2
+    },
+    2: {
+        id: 2,
+        name: "Product 2",
+        price: 200,
+        qty: 1
+    },
+    ..etc
+}
+
+*/
+const indexMap =
+products.reduce((acc, product) => {
+    acc[product.id] = product;
+    return acc;
+}, {})
+
+console.log("\n");
+console.log(indexMap[105]);
